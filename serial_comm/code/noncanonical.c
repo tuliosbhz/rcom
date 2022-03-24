34fdc9b2549e12e6d2e6f3f640b42bd47158d7fc
felisberto888 <101819743+felisberto888@users.noreply.github.com>
2022-03-24 16:51:41 +0000

    Add files via upload
    Lab03 Felisberto

--- serial_comm/code/noncanonical.c
+++ serial_comm/code/noncanonical.c
@@ -5,16 +5,11 @@
 #include <fcntl.h>
 #include <termios.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-
 
 #define BAUDRATE B38400
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
 #define TRUE 1
-#define BUFF_SIZE 255
 
 volatile int STOP=FALSE;
 
@@ -22,10 +17,11 @@ int main(int argc, char** argv)
 {
     int fd,c, res;
     struct termios oldtio,newtio;
-    char buf[BUFF_SIZE];
-    ///dev/ttyS0
-    if (argc < 2) //|| ((strcmp("/tmb/vboxS0", argv[1])!=0) && (strcmp("/tmb/vboxS0", argv[1])!=0) )) 
-    {
+    char buf[255];
+
+    if ( (argc < 2) || 
+  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
+  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
       printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
       exit(1);
     }
@@ -54,12 +50,16 @@ int main(int argc, char** argv)
     newtio.c_lflag = 0;
 
     newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
+    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+
+
 
   /* 
     VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
-    leitura do(s) prï¿½ximo(s) caracter(es)
+    leitura do(s) próximo(s) caracter(es)
   */
+
+
 
     tcflush(fd, TCIOFLUSH);
 
@@ -69,30 +69,19 @@ int main(int argc, char** argv)
     }
 
     printf("New termios structure set\n");
-    char echobuf[BUFF_SIZE];
-    int i=0;
-    while (STOP==FALSE) /* loop for input */
-    {       
-      res = read(fd,buf,1);  /* returns after 1 chars have been input */
-      if(res < 0)
-      {
-        perror("Read on serial file at /dev/ttyS0 failed");
-        STOP=TRUE;
-      } else {
-        echobuf[i] = buf[0]; //Copy character read to buffer of response
-        buf[res]=0;               /* so we can printf... */
-        printf(":%s:%d\t", buf, res);
-        i++;
-      }
-      if (buf[0]=='\0') STOP=TRUE;
+
+
+    while (STOP==FALSE) {       /* loop for input */
+      res = read(fd,buf,255);   /* returns after 5 chars have been input */
+      buf[res]=0;               /* so we can printf... */
+      printf(":%s:%d\n", buf, res);
+      if (buf[0]=='z') STOP=TRUE;
     }
 
-    printf("Content of echo buffer: %s\n", echobuf);
-    res = write(fd,echobuf,sizeof(echobuf));   
-    printf("%d bytes written\n", res);
+
 
   /* 
-    O ciclo WHILE deve ser alterado de modo a respeitar o indicado no guiï¿½o 
+    O ciclo WHILE deve ser alterado de modo a respeitar o indicado no guião 
   */
 
 
