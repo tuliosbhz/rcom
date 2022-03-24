f895fefaf7322c5cf067e4a18868547c135d6397
Tulio <tulio.soares.bhz@gmail.com>
2022-03-24 15:43:29 +0000

    Commit for the Lab03 just simple noncanonical comm

--- serial_comm/code/noncanonical.c
+++ serial_comm/code/noncanonical.c
@@ -53,7 +53,7 @@ int main(int argc, char** argv)
     /* set input mode (non-canonical, no echo,...) */
     newtio.c_lflag = 0;
 
-    newtio.c_cc[VTIME]    = 1;   /* inter-character timer unused */
+    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
     newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
 
   /* 
